b15015d3b09f0bc25ce136252ed7bc8ddfd03f71
Cyns <90992351+SubduedCyns@users.noreply.github.com>
2024-03-22 14:37:01 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+//  CompSci One Start Code
+let balls;
+
+function setup() {
+  var cnv = createCanvas(800, 600);
+  cnv.position((windowWidth-width)/2, 30);
+ 
+}
+
+function draw(){
+  background(20,20,20,20)
+ 
+}
+
